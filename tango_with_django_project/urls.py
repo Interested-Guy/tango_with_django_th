c17994767a4ea4dbef7ba5f951998317fc2ad301
Interested-Guy <1001.tejas@gmail.com>
2022-08-28 23:18:06 +0530

    hello

---
+++ tango_with_django_project/urls.py
@@ -0,0 +1,34 @@
+"""tango_with_django_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from registration.backends.simple.views import RegistrationView
+from django.contrib import admin
+from django.urls import path, include
+from rango import views
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import re_path
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/rango/register_profile'
+
+urlpatterns = [
+    re_path(r'^$', views.index, name='index'),
+    re_path(r'^rango/', include('rango.urls')),
+    re_path(r'^accounts/register/$',MyRegistrationView.as_view(),name='registration_register'),
+    re_path(r'^accounts/', include('registration.backends.simple.urls')),
+    path('admin/', admin.site.urls),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
